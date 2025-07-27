613e8e62ee7dae6793108a9ecbcb78414bcde370
mcobtechnology <magaret@mcobtechnology.com>
2025-07-28 00:14:02 +0100

    update verison to 1.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup(
     name='simple-gedcom',
-    version='1.0.2',
+    version='1.0.3',
     description='A Python module for parsing and analyzing GEDCOM files.',
     author='mcobtechnology',
     long_description=long_description,
